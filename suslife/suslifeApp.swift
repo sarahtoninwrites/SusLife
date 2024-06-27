cf9f2c10163e11a65cc3fde03b9c375699af68d2
Sarah <125749615+sarahtoninwrites@users.noreply.github.com>
2024-06-28 00:18:35 +0530

    Initial Commit

---
+++ suslife/suslifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  suslifeApp.swift
+//  suslife
+//
+//  Created by Sarah Saiyed on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct suslifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
